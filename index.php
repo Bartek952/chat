27e51a3c027b303c63d4bf4796aa940c19dd897b
Bartek <taran06.xd@gmail.com>
2020-03-07 18:02:05 +0100

    add css to start page

--- index.php
+++ index.php
@@ -5,9 +5,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="favicons/mainFavicon.ico" type="image/x-icon">
     <link rel="icon" href="favicons/mainFavicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="css/index.css">
     <title>Chat</title>
 </head>
 <body>
-    <h1>Chat</h1>
+    <h1>
+        <p>Welcome</p>
+        <p>to</p>
+        <p>our</p>
+        <p>Chat!</p>
+    </h1>
 </body>
 </html>
